321095a3edc92e1ae6df0b8a0474fb129232cdf4
nahjjun <nahjjun1125@naver.com>
2026-01-05 12:57:51 +0900

    :tada: Start: Start Egobook Project

---
+++ src/test/java/com/example/egobook_be/EgobookBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egobook_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgobookBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
