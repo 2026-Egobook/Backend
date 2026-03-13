b68f2e401678668d6024a18fc9ed3f200c0673ec
nahjjun <nahjjun1125@naver.com>
2026-03-13 13:53:03 +0900

    :white_check_mark: Test: Guest 회원가입 Integration 테스트 코드

--- src/test/java/com/example/egobook_be/domain/auth/sevice/AuthServiceIntegrationTest.java
+++ src/test/java/com/example/egobook_be/domain/auth/sevice/AuthServiceIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.example.egobook_be.domain.auth.sevice;
 
 import com.example.egobook_be.domain.auth.dto.req.GoogleJoinReqDto;
+import com.example.egobook_be.domain.auth.dto.req.GuestJoinReqDto;
 import com.example.egobook_be.domain.auth.dto.res.JwtTokenResDto;
 import com.example.egobook_be.domain.auth.enums.Provider;
 import com.example.egobook_be.domain.auth.repository.AuthAccountRepository;
 import com.example.egobook_be.domain.auth.repository.RefreshTokenBackupRepository;
+import com.example.egobook_be.domain.home.repository.MissionRepository;
 import com.example.egobook_be.domain.shop.entity.Item;
 import com.example.egobook_be.domain.shop.enums.ItemCategory;
 import com.example.egobook_be.domain.shop.enums.ShopErrorCode;
@@ -14,6 +16,8 @@ import com.example.egobook_be.domain.terms.entity.Term;
 import com.example.egobook_be.domain.terms.enums.TermErrorCode;
 import com.example.egobook_be.domain.terms.enums.TermType;
 import com.example.egobook_be.domain.terms.repository.TermRepository;
+import com.example.egobook_be.domain.terms.repository.UserTermRepository;
+import com.example.egobook_be.domain.user.repository.AbilityRepository;
 import com.example.egobook_be.domain.user.repository.UserRepository;
 import com.example.egobook_be.global.exception.CustomException;
 import com.example.egobook_be.global.util.HashingUtil;
@@ -56,10 +60,14 @@ public class AuthServiceIntegrationTest {
     @Autowired private UserNicknameGenerator userNicknameGenerator;
     @Autowired private RedisUtil redisUtil;
     @Autowired private TermRepository termRepository;
+    @Autowired private AbilityRepository abilityRepository;
+    @Autowired private UserTermRepository userTermRepository;
+    @Autowired private MissionRepository missionRepository;
+
 
     @MockitoBean // 외부 API인 구글 검증 로직만 Mocking 처리 (@MockBean은 deprecated 되었음)
     private GoogleIdTokenVerifier verifier;
-    
+
     @Nested
     @DisplayName("registerGoogle() 메서드 테스트")
     class RegisterGoogleTest{
@@ -104,12 +112,53 @@ public class AuthServiceIntegrationTest {
             assertThat(refreshTokenBackupRepository.count()).isEqualTo(1);
 
             // 3. Redis에 잘 저장되었는지 확인
-            String hashedRefreshToken = hashingUtil.hashingValue(resDto.refreshToken());
-            RedisValue redisValue = redisUtil.getHashedRefreshTokenValue(hashedRefreshToken);
+            String hashedRefreshToken = hashingUtil.hashingValue(resDto.refreshToken()); // Key
+            RedisValue redisValue = redisUtil.getHashedRefreshTokenValue(hashedRefreshToken); // Value
 
             assertThat(redisValue).isNotNull();
             assertThat(redisValue.subject()).isEqualTo(Provider.GOOGLE + ":" + hashingUtil.hashingValue(GOOGLE_SUB));
         }
+    }
+
+    @Nested
+    @DisplayName("registerGuest() 메서드 테스트")
+    class RegisterGuestTest{
+        private final String DEVICE_UID = "test-device-uid";
+
+        @Test
+        @DisplayName("[성공] 사용자 Guest 회원가입")
+        void successRegisterGuest(){
+            // ========= Given =========
+            GuestJoinReqDto reqDto = GuestJoinReqDto.builder().deviceUid(DEVICE_UID).build();
+
+            // ========= When =========
+            JwtTokenResDto resDto = authService.registerGuest(reqDto);
+
+            // ========= Then =========
+            // 1. Dto 반환 결과 확인
+            assertThat(resDto).isNotNull();
+            assertThat(resDto.accessToken()).isNotBlank();
+
+            // 2. DB 확인
+            // (User, UserItems, Ability, UserTerms, Mission, AuthAccount, RefreshTokenBackup)
+            assertThat(userRepository.count()).isEqualTo(1);
+            assertThat(userItemRepository.count()).isNotEqualTo(0); // ItemInitializer로 Item이 초기화되고, 회원가입 시 기본 아이템은 여러개가 설정된다
+            assertThat(abilityRepository.count()).isEqualTo(1);
+            assertThat(userTermRepository.count()).isNotEqualTo(0);  // TermInitializer로 Term이 초기화되고, 회원가입 시 약관은 여러개가 설정된다
+            assertThat(missionRepository.count()).isEqualTo(1);
+            assertThat(authAccountRepository.count()).isEqualTo(1);
+            assertThat(refreshTokenBackupRepository.count()).isEqualTo(1);
+
+            // 3. Redis에 잘 저장되었는지 확인 - key: 해싱된 refresh token, value: 해당 사용자에 대한 메타 데이터
+            String hashedRefreshToken = hashingUtil.hashingValue(resDto.refreshToken()); // Key
+            RedisValue redisValue = redisUtil.getHashedRefreshTokenValue(hashedRefreshToken); // Value
+
+            assertThat(redisValue).isNotNull();
+            assertThat(redisValue.subject()).isEqualTo(Provider.GUEST + ":" + hashingUtil.hashingValue(DEVICE_UID));
+        }
+
+
+
     }
     
     
