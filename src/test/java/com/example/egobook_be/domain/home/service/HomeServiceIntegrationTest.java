8a4b6054cacfe4a844dc060821f12df3cd4d4cb9
nahjjun <nahjjun1125@naver.com>
2026-03-16 13:39:24 +0900

    :white_check_mark: Test: HomeService 홈화면 조회 API 리팩토링 & Unit Test Code

---
+++ src/test/java/com/example/egobook_be/domain/home/service/HomeServiceIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.example.egobook_be.domain.home.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HomeServiceIntegrationTest {
+
+    @Test
+    @DisplayName("[성공 2] 홈 화면에 접속해서 출석 보상 수령 - 2개의 Thread에서 동시에 API 호출")
+    void successGetDailyRewardWithTwoThread() {
+    }
+}
