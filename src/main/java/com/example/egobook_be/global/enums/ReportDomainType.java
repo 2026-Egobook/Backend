b677499ed9646ea9d6e0d920f5d083def411c6a3
nahjjun <nahjjun1125@naver.com>
2026-04-09 17:41:58 +0900

    :sparkles: Feat: 신고 관련 공통 Enum 추가&수정 (ReportDomainType, ReportReason, ReportType)

---
+++ src/main/java/com/example/egobook_be/global/enums/ReportDomainType.java
@@ -0,0 +1,7 @@
+package com.example.egobook_be.global.enums;
+
+public enum ReportDomainType {
+    LETTER,         // 편지 작성
+    LETTER_REPLY,   // 편지 답변 작성
+    QUESTION_ANSWER // 오늘의 질문 답변
+}
--- src/main/java/com/example/egobook_be/global/enums/ReportReason.java
+++ src/main/java/com/example/egobook_be/global/enums/ReportReason.java
@@ -1,6 +1,6 @@
-package com.example.egobook_be.domain.letters.entity;
+package com.example.egobook_be.global.enums;
 
-public enum ReplyReportReason {
+public enum ReportReason {
     ABUSE,          // 비속어/욕설/모욕
     SPAM,           // 광고/스팸
     INAPPROPRIATE,  // 부적절한 콘텐츠
