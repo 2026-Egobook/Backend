55145d1dd4c8e86bc8de9ea00679a8845205fcc8
nahjjun <nahjjun1125@naver.com>
2026-04-11 11:52:34 +0900

    :memo: Docs: UserStatus의 'SUSPEND'는 편지나 오늘의 질문 도메인에 대한 제재가 아닌 완전한 정지임을 명시

--- src/main/java/com/example/egobook_be/domain/user/enums/UserStatus.java
+++ src/main/java/com/example/egobook_be/domain/user/enums/UserStatus.java
@@ -5,5 +5,5 @@ public enum UserStatus {
     DORMANT,        // 휴면
     WITHDRAW_PENDING,// 탈퇴 대기 (완전 삭제 전 유예 기간)
     WITHDRAW,         // 탈퇴됨
-    SUSPENDED,  // 정지됨
+    SUSPENDED,  // 정지됨 (편지, 오늘의 질문 답변에 대한 제재가 아니라, 문제가 생겼을 경우 계정 자체를 완전 정지시키는 경우)
 }
