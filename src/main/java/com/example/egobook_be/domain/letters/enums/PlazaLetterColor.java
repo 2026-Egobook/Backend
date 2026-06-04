fc9e898a91e274551d847ccb9ea4fee3a842a176
nahjjun <nahjjun1125@naver.com>
2026-06-04 20:54:29 +0900

    :bug: Fix: 편지지 이름 .png 설정 오류

--- src/main/java/com/example/egobook_be/domain/letters/enums/PlazaLetterColor.java
+++ src/main/java/com/example/egobook_be/domain/letters/enums/PlazaLetterColor.java
@@ -1,21 +1,19 @@
 package com.example.egobook_be.domain.letters.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum PlazaLetterColor {
-    WHITE(0),  // 기본 하얀색, 가격 0원
-    PINK(75),  // 핑크색, 가격 75원
-    GREEN(100),  // 초록색, 가격 100원
-    BLUE(150),  // 파란색, 가격 150원
-    PURPLE(175);  // 보라색, 가격 175원
+    WHITE(0, "White.png"),  // 기본 하얀색, 가격 0원
+    PINK(75, "Pink.png"),  // 핑크색, 가격 75원
+    GREEN(100, "Green.png"),  // 초록색, 가격 100원
+    BLUE(150, "Blue.png"),  // 파란색, 가격 150원
+    PURPLE(175, "Purple.png");  // 보라색, 가격 175원
 
     private final int price;
-
-    PlazaLetterColor(int price) {
-        this.price = price;
-    }
-
-    public int getPrice() {
-        return price;
-    }
+    private final String imageName;
 
 }
 
