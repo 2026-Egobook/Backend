7dcde082da14985a53ed19ce5106845738f230a5
7iniyo <  >
2026-07-23 15:01:15 +0900

    🐛  Fix: ShopErrorCode PROFILE_IMAGE_LOAD_FAILED 중복 선언 제거

--- src/main/java/com/example/egobook_be/domain/shop/exception/ShopErrorCode.java
+++ src/main/java/com/example/egobook_be/domain/shop/exception/ShopErrorCode.java
@@ -14,7 +14,6 @@ public enum ShopErrorCode implements BaseErrorCode {
     INSUFFICIENT_INK_TO_BUY_ITEM(HttpStatus.BAD_REQUEST, "잉크가 부족하여 해당 아이템을 구매할 수 없습니다."),
     ITEM_NOT_PURCHASED(HttpStatus.BAD_REQUEST, "아이템이 구매되지 않았습니다."),
     INVALID_ITEM_STATUS(HttpStatus.BAD_REQUEST,"유효하지 않은 상태 값입니다."),
-    PROFILE_IMAGE_LOAD_FAILED(HttpStatus.BAD_REQUEST, "프로필 이미지 불러오기에 실패했습니다."),
     /*
      * 404 NOT FOUND
      */
